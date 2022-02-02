c62bb4fc6b288fa6076a3ef9de01019b22c39e0a
Cristopher Martins <cristopher.martins@alviere.com>
2022-02-02 18:36:45 +0000

    update switch layer to enums

---
+++ keyboards/crkbd/keymaps/cristopheryusuke/enums.h
@@ -0,0 +1,11 @@
+// [CRKBD layers Init] -------------------------------------------------------//
+typedef enum {
+    _QWERTY,
+    _NUM,
+    _NAV
+    _SYM,
+    _GAME,
+    _MEDIA
+}CRKBD_LAYERS;
+
+extern enum CRKBD_LAYERS crkbd_layers;
