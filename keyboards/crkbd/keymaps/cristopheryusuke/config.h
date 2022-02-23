8385d35e41bf7663d3ad20bca21672ec24c5ded3
Cristopher Martins <cristopher.martins@alviere.com>
2022-02-23 11:43:20 +0000

    adding new logo and luna qmk pet

--- keyboards/crkbd/keymaps/cristopheryusuke/config.h
+++ keyboards/crkbd/keymaps/cristopheryusuke/config.h
@@ -25,6 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Select hand configuration */
 
 #define MASTER_LEFT
+#define SPLIT_USB_DETECT
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
@@ -32,15 +33,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 200
 
+#define USE_SERIAL_PD2
+#define OLED_BRIGHTNESS 120
+#define OLED_TIMEOUT 60000
+
+#define SPLIT_WPM_ENABLE
+#define SPLIT_OLED_ENABLE
+
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
     #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-    #define RGBLIGHT_EFFECT_SNAKE
-    #define RGBLIGHT_EFFECT_KNIGHT
-    #define RGBLIGHT_EFFECT_CHRISTMAS
     #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-    #define RGBLIGHT_EFFECT_RGB_TEST
     #define RGBLIGHT_EFFECT_ALTERNATING
     #define RGBLIGHT_EFFECT_TWINKLE
     #define RGBLIGHT_LIMIT_VAL 120
